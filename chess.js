90e7df13435070c1e43bcb39f990395eb3352cd6
Mathew Hawley <mathew@mathewhawley.com>
2017-02-11 12:34:49 +0000

    Set up board, render sprites

--- chess.js
+++ chess.js
@@ -1,1 +1,29 @@
-console.log(jQuery)
+const mountNode = $('#board')
+
+const createBoard = (height, width) => {
+  const board = [
+    ['br','bkn','bb','bq','bk','bb','bkn','br'],
+    _.times(8, () => 'bp'),
+    _.times(8, () => 'empty'),
+    _.times(8, () => 'empty'),
+    _.times(8, () => 'empty'),
+    _.times(8, () => 'empty'),
+    _.times(8, () => 'wp'),
+    ['wr','wkn','wb','wq','wk','wb','wkn','wr'],
+  ]
+  return board
+}
+
+const board = createBoard(8, 8)
+
+const render = (board) => {
+  board.forEach((row, i) => {
+    row.forEach((cell, j) => {
+      const color = (i + j) % 2 ? 'black' : 'white'
+      const html = `<div class="cell cell--${color} cell--${i}-${j} cell--${cell}"></div>`
+      $(html).appendTo('#board')
+    })
+  })
+}
+
+render(board)
