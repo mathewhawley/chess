55c9e51daadd37e7601961fcac28eaf3931268b2
oli cairns <oli@olis-MacBook-Air.local>
2017-02-12 12:27:15 +0000

    fixed diagonal jumping function, added queen moves function and additional comments

--- moves.js
+++ moves.js
@@ -1,4 +1,6 @@
-//all nojump functions check that the candidate move does not involve an illegal 'jump' 
+//All functions in this file are used to check if candidate moves are valid. 
+
+//nojump helper functions check that the candidate move does not involve an illegal 'jump' over non-empty squares
 function noVerticalJump(){
   //if moving forward
   if (state.origin[0] > state.dest[0]) {
@@ -20,7 +22,8 @@ function noVerticalJump(){
   //after perfoming either test, if all cells are empty, return true
   return true;
 }
-//same as above for horizontal jumps
+
+//same as above for horizontal moves
 function noHorizontalJump(){
   if (state.origin[1] > state.dest[1]) {
     for(var i=state.dest[1]+1; i < state.origin[1]; i++){
@@ -30,7 +33,6 @@ function noHorizontalJump(){
     }
   }
   else{
-    console.log('check horizontal left')
     for(var i=state.origin[1]+1; i < state.dest[1]; i++){
       if(state.board[state.dest[0]][i]!='empty'){
         return false;
@@ -40,6 +42,7 @@ function noHorizontalJump(){
   return true;
 }
 
+//Hard to read. Loops through squares connecting origin and destination and checks for non-empty squares, according to for different cases.  
 function noDiagonalJump(){
   if(state.dest[0]< state.origin[0] && state.dest[1] <  state.origin[1]){
     for(var i= 1; i < state.origin[0] - state.dest[0]; i++){
@@ -48,36 +51,35 @@ function noDiagonalJump(){
       }
     }
   }
-  else if(state.dest[0]< state.origin[0] && state.dest[1] <  state.origin[1]){
+  else if(state.dest[0] > state.origin[0] && state.dest[1] >  state.origin[1]){
     for(var i= 1; i < state.dest[0] - state.origin[0]; i++){
       if(state.board[state.origin[0]+i][state.origin[1]+i]!='empty'){
         return false;
       }
     }
   }
+  else if(state.dest[0]< state.origin[0] && state.dest[1] >  state.origin[1]){
+    for(var i= 1; i < state.origin[0] - state.dest[0]; i++){
+      if(state.board[state.dest[0]+i][state.origin[1]+i]!='empty'){
+        return false;
+      }
+    }
+  }
   else{
-    return true;
+    for(var i= 1; i < state.dest[0] - state.origin[0]; i++){
+      if(state.board[state.origin[0]+i][state.dest[1]+i]!='empty'){
+        return false;
+      }
+    }
   }
   return true;
-  // else if(dest[0]< state.origin[0] && dest[1] >  state.origin[1]){
-  //   for(var i= 1; i < dest[0] - state.origin[0]; i++){
-  //     if(state.board[state.origin[0]+i][state.origin[1]+i]!='empty'){
-  //       return false;
-  //     }
-  //   }
-  // }
-  // else{
-  //   for(var i= 1; i < dest[0] - state.origin[0]; i++){
-  //     if(state.board[state.origin[0]+i][state.origin[1]+i]!='empty'){
-  //       return false;
-  //     }
-  //   }
-  // }
 }
 
 function validRookMove() {
+  //Piece cannot have changed both row and column
   if (state.origin[0] !== state.dest[0] && state.origin[1] !== state.dest[1]) {
     return false
+  //Cannot have jumped over any pieces
   } else if (state.origin[0] === state.dest[0]) {
     return noHorizontalJump();
   } else {
@@ -88,11 +90,14 @@ function validRookMove() {
 function validKnightMove() {
   var distOne = Math.abs(state.origin[0] - state.dest[0]);
   var distTwo = Math.abs(state.origin[1] - state.dest[1]);
+  //minimum distance between starting and ending square == 1 and maximum == 2 (moved in 2x1 l shape)
   return Math.max(distOne,distTwo) === 2 && Math.min(distOne, distTwo) ===1 ;
 }
 
 function validBishopMove(){
+  //Absolute difference between origin and destination columns and rows must be the same (diagonal move)
   if (Math.abs(state.origin[0] - state.dest[0]) === Math.abs(state.origin[1] - state.dest[1])){
+    //piece cannot have 'jumped' over non-empty squares
     return noDiagonalJump();
   }
   else{
@@ -100,19 +105,14 @@ function validBishopMove(){
   }
 }
 
-
+//King cannot have moved more than 1 square in other directions.
 function validKingMove(){
-  if (Math.abs(state.origin[0] - state.dest[0]) >1) {
-    return false;
-  } else if (Math.abs(state.origin[1] - state.dest[1]) >1) {
-    return false;
-  } else {
-    return true
-  }
+  return (Math.abs(state.origin[0] - state.dest[0]) < 2) && (Math.abs(state.origin[1] - state.dest[1]) < 2) 
 }
 
+//Reusing bishop and rook functions.
 function validQueenMove(){
-  return true
+  return validBishopMove() || validRookMove()
 }
 
 function validBlackPawnMove(){
@@ -125,18 +125,20 @@ function validBlackPawnMove(){
   if(state.origin[0]== 1 && state.dest[0] == 3 && state.dest[1] === state.origin[1] && state.board[state.dest[0]][state.dest[1]]==='empty'){
     return true;
   }
-
   return false
 }
 
 function validWhitePawnMove(){
+  //moved 1 forward to empty square
   if(state.origin[0]-state.dest[0] === 1 && state.dest[1] === state.origin[1] && state.board[state.dest[0]][state.dest[1]]==='empty'){
     return true;
   }
+  //moved 1 forward, 1 diagonal and captured piece
   if(state.origin[0]-state.dest[0] === 1 && Math.abs(state.dest[1]-state.origin[1]) ===1 && state.board[state.dest[0]][state.dest[1]][0]==='b'){
     return true;
   }
 
+  //moved two forward from starting row
   if(state.origin[0]== 6 && state.dest[0] == 4 && state.dest[1] === state.origin[1] && state.board[state.dest[0]][state.dest[1]]==='empty'){
     return true;
   }
